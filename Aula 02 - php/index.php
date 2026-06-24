10a7d7b2000f49b5bf66780173a0f7c6f3958004
emanuellyslobato-debug <emanuelly.slobato@aluno.edu.es.gov.br>
2026-06-24 10:59:26 -0300

    Create index.php

---
+++ Aula 02 - php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang "pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title> Aula 02 php - html </title>
+    <head>
+    <body>
+        <?php
+            $nome = "Creuza";
+            $idade = "67";
+            echo "Meu nome é ".$nome." e tenho ".$idade." anos.";
+        ?>
+    </body>
+</html>  
